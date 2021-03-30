f856e721465289c751e5e691a1abd785aef2848e
riya <54314240+riya69@users.noreply.github.com>
2021-03-30 10:02:11 +0200

    Update Program.cs

--- Discord Cache Viewer/Program.cs
+++ Discord Cache Viewer/Program.cs
@@ -6,6 +6,12 @@ namespace Discord_Cache_Viewer
 {
     internal struct Program
     {
+        #region Variables
+        /// <summary>
+        /// Time to wait (ms) before the program closes after a successful conversion.
+        /// </summary>
+        private const short kill = 10000;
+        #endregion
         /// <summary>
         /// Startup function.
         /// </summary>
@@ -15,8 +21,8 @@ namespace Discord_Cache_Viewer
             // Begin the processing after a new console line has been received from the user
             begin_processing(Console.ReadLine());
             Console.WriteLine("Processed all files, closing in 10 seconds");
-            // Wait 10,000ms (10s)
-            await Task.Delay(10000);
+            // Wait a few seconds before closing
+            await Task.Delay(kill);
             // Collect the garbage and close
             GC.Collect();
             Environment.Exit(0);
@@ -33,14 +39,14 @@ namespace Discord_Cache_Viewer
             // Constants for more efficient memory management
             const string header = "PNG", extension = ".png";
             const ushort index = 0;
-            for (short i = 0; i < files.Length; i++) // UShort has a greater max length than a signed Short.
+            for (ushort i = 0; i < files.Length; i++)
             {
                 // Cache the file path
                 var filePath = files[i];
                 // We only have to read the first line and check if it has the PNG header.
                 // This also saves some processing time, as we don't have to check the entire file for "PNG", and it reduces false positives.
                 if (File.ReadAllLines(filePath)[index].Contains(header))
-                    Console.WriteLine($"{filePath} - Added .png extension");
+                    Console.WriteLine($"{filePath} - Added {extension} extension");
                 // "Move" => "Rename", alright then Microsoft.
                 File.Move(filePath, filePath + extension);
             }
